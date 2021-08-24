05c7391a24a18d33a8f05e82a1a5f0aae83a290b
Joel Dsouza <64890495+joel06-dsouza@users.noreply.github.com>
2021-08-24 19:53:27 +0530

    Add files via upload

---
+++ WHICHDIV.py
@@ -0,0 +1,11 @@
+t=int(input())
+while t > 0:
+    t=t-1
+    n=int(input())
+    if n >=2000:
+        print(1)
+    elif n>=1600 and n<=2000:
+        print(2)
+    else:
+        print(3)
+        
